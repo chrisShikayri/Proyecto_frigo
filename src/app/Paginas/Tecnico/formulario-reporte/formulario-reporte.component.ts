1e5ad1d2229126ef2f882cf4ec8c07f7ea82d665
Dano-19 <dam.cardenas@yavirac.edu.ec>
2025-05-27 18:35:57 -0500

    Carpetas

---
+++ src/app/Paginas/Tecnico/formulario-reporte/formulario-reporte.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-reporte',
+  imports: [],
+  templateUrl: './formulario-reporte.component.html',
+  styleUrl: './formulario-reporte.component.scss'
+})
+export class FormularioReporteComponent {
+
+}
