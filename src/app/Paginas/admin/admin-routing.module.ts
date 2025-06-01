79fc53568c79f27f50e79a8c91ff22106318e247
Jossy2005 <jab.mena@yavirac.edu.ec>
2025-06-01 16:08:46 -0500

    tickets

--- src/app/Paginas/admin/admin-routing.module.ts
+++ src/app/Paginas/admin/admin-routing.module.ts
@@ -1,5 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { TicketsComponent } from './tickets/tickets.component';
 
 const routes: Routes = [
+  { path: 'tickets', component: TicketsComponent },
+  // otras rutas
 ];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdminRoutingModule { }
--- src/app/Paginas/admin/admin.module.ts
+++ src/app/Paginas/admin/admin.module.ts
@@ -1,1 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+// PrimeNG
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+// Routing del módulo admin
+import { AdminRoutingModule } from './admin-routing.module';
+
+// Componentes del módulo admin
+import { TicketsComponent } from './tickets/tickets.component';
+
+@NgModule({
+  declarations: [
+    TicketsComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    AdminRoutingModule,
+    CalendarModule,
+    DropdownModule,
+    InputTextModule,
+    ButtonModule
+  ]
+})
+export class AdminModule { }
