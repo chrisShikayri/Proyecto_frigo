01a6838877fdcf1a51cd6e7d8440e667af773454
tell1to <hmd.murillo.edu.ec>
2025-06-01 10:50:46 -0500

    pedido-cliente-component

---
+++ src/app/Paginas/cliente/pedido-cliente/pedido-cliente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-cliente',
+  imports: [],
+  templateUrl: './pedido-cliente.component.html',
+  styleUrl: './pedido-cliente.component.scss'
+})
+export class PedidoClienteComponent {
+
+}
